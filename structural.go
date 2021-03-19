b059610f60fb2b1dcb020da95af5339e5acdaf46
beeceej <brian@beeceej.com>
2021-03-19 16:40:11 -0400

    Docs: Note purposes of types in structural.go

--- structural.go
+++ structural.go
@@ -1,13 +1,19 @@
 package structural
 
 type (
-	JSON  struct{}
-	XML   struct{}
-	Route struct{}
-	API   struct{}
-)
+	// JSON signals that structural should interpret the field as JSON
+	// This is only used a a type level identifier; a note for structural
+	// so that it can build the correct code
+	JSON interface{}
 
-//func x() {
-//	d, err := strconv.Atoi("10")
-//
-//}
+	// XML signals that structural should interpret the field as XML
+	// This is only used a a type level identifier; a note for structural
+	// so that it can build the correct code
+	XML interface{}
+
+	// Route signals that structural should consider a type as a route
+	Route interface{}
+
+	// API signals that structural should consider a type as an API
+	API interface{}
+)
